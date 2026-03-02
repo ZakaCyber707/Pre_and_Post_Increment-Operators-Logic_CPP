b1c2530424cfee377e2243ec40e5bbdfa0ac982c
Muhammad Zaka ILLAHi <babrumbabrum12@gmail.com>
2026-03-02 23:31:12 +0500

    Implement pre and post increment demonstration
    "The code shows how ++a (pre-increment) adds to the value before printing, and explains how a++ (post-increment) prints the current value before adding to it."

---
+++ Pre_and_Post increment.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int a=5;
+	
+	cout<<++a; //y (pre increment) hy pehly jma kro phir value ko variable mn dal kr console mn show krwao
+				//this is (pre increment) ,first add then display
+	
+	
+	
+	//cout<<a++<<endl; //y (post increment) hy pehly (a) ko console pr show krwao phir agle bari mn value ko add kro 
+	//cout<<a;		//	//this is (pre increment) ,first display then add
+}
+
